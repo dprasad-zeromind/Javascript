0c491da03aa5a6efdd3bd03f492605d85a905c06
Durga Prasad <mdprsd9@gmail.com>
2026-02-23 17:36:39 +0530

    java practice

---
+++ script.js
@@ -0,0 +1,84 @@
+let js= "amazing";
+if(js==="amazing")alert("Java script is Fun!");
+
+//values and variables   
+let country = "india";
+let continent ="Andhra Pradesh";
+let population =53.448;
+
+console.log(country,"\n"+continent,"\n"+population);
+
+//Data types
+
+let isIsland = false
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//let, const and var
+language ="Telugu";
+const ap= "Andhra Pradesh";
+const tG="Telangana";
+
+//Basic Operator's
+
+console.log(population / 2);
+population ++;
+console.log(population);    
+console.log(population>6);
+console.log(population<33);
+
+
+const description= continent +' is part of ' + country+' and its population of '+population+' million people speck '+ language;
+console.log(description);
+
+//Literals
+console.log(`${continent} is part of ${country} and its population of ${population} million people speck ${language}`);
+
+//math operator's
+const year=2026;
+const agePrasad=year-1990;
+const ageSarada=year-1992;
+
+console.log(agePrasad+ "\n"+ ageSarada);
+
+// Assignment operator's   
+let x=10+5;
+x+=10;
+x*=4;
+x++;
+x--;
+console.log(x);
+
+//comparison oprator's
+
+console.log(agePrasad>ageSarada);
+console.log(agePrasad>=18);
+const isFullage= agePrasad>=18;
+console.log(year-1991>year-2018);
+
+let a,b;
+a=b= 25-10-5;
+console.log(a,b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
