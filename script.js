d8118082986dd9c19646d4a038577aea66b1a5a5
Durga prasad <mdprsd9@gmail.com>
2026-08-31 22:34:55 +0530

    a

--- script.js
+++ script.js
@@ -112,24 +112,24 @@
 //         console.log(num,'Is Not Multiple Of 5');
 //     }
 
-    let score=prompt('Enter your score(0-100)');
-    let grade;
-if(score >= 90 && score <= 100){
+let score = prompt('Enter your score(0-100)');
+let grade;
+if (score >= 90 && score <= 100) {
     grade = "A";
-}else if (score >=70 && score<= 89){
-    grade ="B";
-}else if(score >=60 && score<=69){
-    grade ="C";
-}else if(score >=50 && score<=59){
-    grade ="D";
-}else if (score >= 0 && score<=49){
-        grade ="Fail";
-}else if (score >=100){
-    grade="Stupid Please Enter corrct Markes!";
+} else if (score >= 70 && score <= 89) {
+    grade = "B";
+} else if (score >= 60 && score <= 69) {
+    grade = "C";
+} else if (score >= 50 && score <= 59) {
+    grade = "D";
+} else if (score >= 0 && score <= 49) {
+    grade = "Fail";
+} else if (score >= 100) {
+    grade = "Stupid Please Enter corrct Markes!";
 }
 
-console.log("According your score, Your Grade Was :",grade);
-console.log=prompt("According your score, Your Grade Was : ",grade);
+console.log("According your score, Your Grade Was :", grade);
+console.log = prompt("According your score, Your Grade Was : ", grade);
 
 
 
